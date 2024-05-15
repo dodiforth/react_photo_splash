06a641f305321dea332f718073e7519e89853ace
Dowon Kim <dowoniscool@yahoo.com>
2024-05-15 23:46:34 +0200

    chore: Add CommonSearchBar component to index page
    The code changes in `index.tsx` add the `CommonSearchBar` component to the index page. This enhancement improves the user experience by providing a search bar for users to search for content.

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,5 @@
 import CommonHeader from "@/components/common/header/CommonHeader";
+import CommonSearchBar from "@/components/common/searchBar/CommonSearchBar";
 import styles from "./styles/index.module.scss";
 
 function index() {
@@ -7,6 +8,7 @@ function index() {
       {/*공통 헤더 UI 부분 - Common Header UI Section*/}
       <CommonHeader />
       {/*공통 네비게이션 UI 부분 - Common Navigation UI Section*/}
+      {/*메인 페이지 컨텐츠 UI 부분 - Main Page Contents UI Section*/}
       <div className={styles.page__contents}>
         <div className={styles.page__contents__introBox}>
           <div className={styles.wrapper}>
@@ -16,6 +18,7 @@ function index() {
               Supported by creators from all regions.
             </span>
             {/*검색창 UI부분 - Search Bar UI Section*/}
+            <CommonSearchBar />
           </div>
         </div>
         <div className={styles.page__contents__imageBox}></div>
