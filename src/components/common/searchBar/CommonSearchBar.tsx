9b94f5396ca2bc3f903ba429a5202b507fdfadd8
Dowon Kim <dowoniscool@yahoo.com>
2024-05-17 16:48:27 +0200

    feat: Update CommonSearchBar component - Searching is possible !
    This commit adds functionality to the CommonSearchBar component in CommonSearchBar.tsx. It introduces state management using the useState hook to handle user input and search functionality. The onChange event handler updates the text state with the input value, while the onSearch event handler sets the search state based on the input value.
    Additionally, the handleKeyDown event handler allows users to trigger the search by pressing the Enter key. This enhancement improves the usability of the search bar and provides a better user experience.

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 import styles from "./CommonSearchBar.module.scss";
+import { useRecoilState } from "recoil";
+import { searchState } from "@/store/atoms/searchState";
 
 function CommonSearchBar() {
+  const [search, setSearch] = useRecoilState(searchState);
+  const [text, setText] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onSearch = () => {
+    if (text === "") {
+      // If the search bar is empty, set the default search value
+      setSearch("Korea");
+    } else {
+      setSearch(text);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (text === "") {
+        // If the search bar is empty, set the default search value
+        setSearch("Korea");
+      } else {
+        setSearch(text);
+      }
+    }
+  };
   return (
     <div className={styles.searchBar}>
       <div className={styles.searchBar__search}>
@@ -8,8 +36,11 @@ function CommonSearchBar() {
           type="text"
           placeholder="Search high-resolution image"
           className={styles.searchBar__search__input}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+          value={text}
         />
-        <img src="src/assets/icons/icon-search.svg" alt="" />
+        <img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
       </div>
     </div>
   );
